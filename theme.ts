0a5b09575bbb013084eedf0d30a640ab649bd7aa
Gaspi <gasparelpi@gmail.com>
2023-05-17 03:04:03 -0300

    chakraui setup

---
+++ theme.ts
@@ -0,0 +1,18 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      "html, body, #root, main": {
+        height: "100%",
+        width: "100%",
+        bg: "#f1f2f3",
+      },
+    },
+  },
+  colors: {
+    
+  },
+});
+
+export default theme;
